521607bfda82d3f5b16bd2db1824e7017dcd3c94
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-09-15 16:28:43 +0300

    A function that replaces all accurences of an element by another in a new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if len(my_list) == 0:
+        return
+    new_list = my_list[:]
+
+    for i in range(len(new_list) - 1):
+        if new_list[i] == search:
+            new_list[i] = replace
+
+    return new_list

80b190bc8611cc1bf67008190d45430b73162a84
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-09-27 01:03:07 +0300

    A class Square that defines a square and has a private instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Square():
+    """
+    a class that defines a square
+
+    """
+    def __init__(self, size):
+        """
+        initialization method
+
+        this method is used to instantiate this classe's attributes
+
+        Args:
+            size: a private attribute representing the size of the square
+        """
+        self.__size = size

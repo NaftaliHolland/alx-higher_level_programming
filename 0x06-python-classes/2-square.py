7de9a838baa0507b816821d19552f570fd7c2f25
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-09-27 02:51:23 +0300

    A class that defines a square and checks the values of size before assigning

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines a Square class
+
+The class will have some properties
+
+"""
+
+
+class Square:
+    """A class that defines a square
+
+    Args:
+    No public arguments yet
+
+    """
+    def __init__(self, size=0):
+        """Initializes size after checking some conditions.
+
+        This method ensures that size is an integer and is not less than 0
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size

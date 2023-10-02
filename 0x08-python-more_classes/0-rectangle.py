b7eb6a47494c2b96d6b1a283f1397487646aba36
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-02 18:45:27 +0300

    Implemented an empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+"""This class defines a rectangle
+
+empty for now
+
+"""
+
+
+class Rectangle:
+    """this class will contains attributes and methods for a rectangle
+
+    it is still empty for now
+    """
+    pass

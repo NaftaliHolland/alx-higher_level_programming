aa12d79918407089a408797a84795711383a9d86
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-04 18:09:52 +0300

    Removed documentation to make the file 3 line long

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-"""This module contains only one function copy_list"""
-
-
 def copy_list(l):
-    """creates a copy of list l
-
-    Returns: copy of l
-    """
-
     return l[:]

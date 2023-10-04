943e22a899428a72e1743ba60cb2ee6f79e52817
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-04 18:08:27 +0300

    A function that retuns a copy of a list

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains only one function copy_list"""
+
+
+def copy_list(l):
+    """creates a copy of list l
+
+    Returns: copy of l
+    """
+
+    return l[:]

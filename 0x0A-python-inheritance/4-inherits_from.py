567b9118078299968347c74b425f7e5aaddb0e43
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-09 18:10:10 +0300

    Added a check to ruturn false if obj is an instance of a_class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """Checks if obj is an instance of a class that inherits a_class
     directly or indirectly"""
 
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class

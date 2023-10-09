1ddbc9f2c0b4075db5f351f5751658ec8bf390dd
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-09 18:46:44 +0300

    A added an instance method that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module has an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Has an instance method area()"""
+
+    def area(self):
+        """Raise:
+                area() is not implemented
+        """
+        raise Exception("area() is not implemented")

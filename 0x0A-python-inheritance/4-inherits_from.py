3c64a8be4dba9219e10239c29def52b1e3aa75a1
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-09 18:04:06 +0300

    A function that checks if an object is an instance of a class that inherits from the given class eigher directly or indirectly

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module has only one function inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if obj is an instance of a class that inherits a_class
+    directly or indirectly"""
+
+    return issubclass(type(obj), a_class)

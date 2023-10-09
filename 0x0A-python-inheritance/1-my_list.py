073261460b002c9ae2446f6e8bedc6f8c4aebe1a
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-09 17:05:11 +0300

    Added some space after documentation to comply with the pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This module has a class that inherits the builtin list class"""
 
+
 class MyList(list):
     """This class inherits the list class and has
     one method"""
 
     def print_sorted(self):
         """Prints the list values in a sorted manner"""
+
         print(sorted(self))

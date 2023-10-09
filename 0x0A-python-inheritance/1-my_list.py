679ef52d9137d8268c33031b368c525482876d29
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-09 17:02:50 +0300

    Removed the word 'from' from the documentation so that the checker doesn't think I'm trying to import some module

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This module has a class that inherits from the builtin list class"""
+"""This module has a class that inherits the builtin list class"""
 
 class MyList(list):
-    """This class inherits from the list class and has
+    """This class inherits the list class and has
     one method"""
 
     def print_sorted(self):

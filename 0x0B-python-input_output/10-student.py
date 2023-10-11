78a65caf8e0c556e64458e91069d0610b7136fc1
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-10-11 04:41:43 +0300

    Modified the to_json() method to retrieve the a json dictionary with specified keys

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module documentation"""
+
+
+class Student:
+    """A student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes the instance attributes"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student
+        for attributes specified in attrs
+        """
+
+        if attrs:
+            new_dict = dict()
+            for attr in attrs:
+                if attr not in self.__dict__.keys():
+                    continue
+                new_dict[attr] = self.__dict__[attr]
+
+            return new_dict
+
+        return self.__dict__

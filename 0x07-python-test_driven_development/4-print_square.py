627c8768c427ae5d52f132f297a7c9ea17c1b74d
Naftali Holland <naftaliholland01@gmail.com>
2023-10-29 13:06:29 +0300

    A function that prints a square of '#'

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module documention"""
+
+
+def print_square(size):
+    """Prints a square"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 9")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for i in range(size):
+            print("#", end="")
+        print()

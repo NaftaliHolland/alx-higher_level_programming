9ebb81aea53fff444305cad810340174d8da4517
Naftali Holland <naftaliholland01@gmail.com>
2023-10-30 11:25:44 +0300

    Added a to_dict method in the Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,13 +5,37 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """A square class that inherits from Rectangle"""
-
     def __init__(self, size, x=0, y=0, id=None):
         """initializes the attributes"""
         super().__init__(size, size, x, y, id)
 
-        def __str__(self):
-            """overiding the string method"""
-            return """
-                [Square] ({}) {}/{} - {}
-                """.format(self.id, self.__x, self.__y, self.__width)
+    @property
+    def size(self):
+        """getter for size"""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of size"""
+        self.__width = value
+        self.__height = value
+
+    def __str__(self):
+        """overiding the string method"""
+
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id,
+                self.x,
+                self.y,
+                self.width
+                )
+
+    def to_dictionary(self):
+        """retuns the dictionary representation of a Square"""
+
+        return {
+                "id": self.x,
+                "size": self.width,
+                "x": self.x,
+                "y": self.y
+                }

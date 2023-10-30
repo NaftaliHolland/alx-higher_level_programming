afc8438b5b5d2f92e59bcc61f8b123435b7b0405
Naftali Holland <naftaliholland01@gmail.com>
2023-10-30 19:59:54 +0300

    Changed the if condition

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,7 @@
 def add_attribute(obj, attr_name, attr_value):
     """ Adds attribute to an object """
 
-    if not hasattr(obj, attr_name):
+    if hasattr(obj, "__dict__"):
         setattr(obj, attr_name, attr_value)
     else:
         raise TypeError("can't add new attribute")

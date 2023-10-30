1e70d85aca86e6339a37a810b323716a0ddbd29d
Naftali Holland <naftaliholland01@gmail.com>
2023-10-30 11:59:23 +0300

    Added the to_json_string method that returns the JSON string representation of the object

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the base module"""
+import json
 
 
 class Base:
@@ -15,3 +16,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the json string representation of list_dictionaries"""
+
+        if list_dictionaries is None or len(list_dictionaries) < 1:
+            return "[]"
+
+        return json.dumps(list_dictionaries)

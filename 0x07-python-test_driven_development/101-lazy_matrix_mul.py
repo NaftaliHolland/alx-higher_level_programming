ae81e64b3532714a87185ecc6fafba35e9146e20
Naftali Holland <naftaliholland01@gmail.com>
2023-10-30 04:29:00 +0300

    Using numpy to multiply a matrix

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This file has one function"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Uses numpy to multiply a matrix"""
+
+    return np.matmul(m_a, m_b)

898ca08020faa67278291dc7f69b202bad25ccfa
Naftali Holland <naftaliholland01@gmail.com>
2023-11-19 08:36:07 +0300

    A script that creates a table and inserts valus

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Creates a table 'second_table' and inserts values
+
+CREATE TABLE IF NOT EXISTS `second_table`(
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT
+	);
+
+INSERT INT `second_table`
+VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);

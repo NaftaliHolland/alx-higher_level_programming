26eeb3fcad34a571354779d1811f7b7eaee351ea
Naftali Holland <naftaliholland01@gmail.com>
2023-12-13 08:46:33 +0300

    A script that converts a database to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,16 @@
+-- Converts hbtn_0c_0 database to utf8
+
+USE `hbtn_0c_0`;
+
+ALTER DATABASE `hbtn_0c_0`
+CHARACTER SET utf8mb4
+COLLATE utf3mb4_unicode_ci;
+
+ALTER TABLE `first_table`
+CHARACTER SET utf8mb4
+COLLATE utf3mb4_unicode_ci;
+
+ALTER TABLE `hbtn_0c_0`
+MODIFY `name` TEXT
+CHARACTER SET utf8mb4
+COLLATE utf3mb4_unicode_ci;

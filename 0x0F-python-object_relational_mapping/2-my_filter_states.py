49d7320f5a50c3032e2db6712c9a226f517f3f91
Naftali Holland <naftaliholland01@gmail.com>
2023-12-21 01:31:20 +0300

    A script that takes an argument and displays all values in the table

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Takes an argument and displays all values that matche the argument """
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    user_name = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db_conn = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                passwd=pwd,
+                db=db_name,
+                charset="utf8"
+            )
+
+    curs = db_conn.cursor()
+    curs.execute("""SELECT * FROM states WHERE name = %s""", (state_name,))
+
+    states_found = curs.fetchall()
+
+    for _ in states_found:
+        print(_)
+
+    curs.close()
+    db_conn.close()

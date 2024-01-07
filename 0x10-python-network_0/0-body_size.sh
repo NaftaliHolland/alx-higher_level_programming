8a43b2f88c0d136350d1f7acaaef80c10a0fe92b
Naftali Holland <naftaliholland01@gmail.com>
2024-01-08 01:07:52 +0300

    Changed grep's search text

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL and displays the size of the body of the response
-curl -sI $1 | grep "content-length"| awk '{print $2}'
+curl -sI $1 | grep "Content-Length"| awk '{print $2}'

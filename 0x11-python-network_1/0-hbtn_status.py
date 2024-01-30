e1f6447e2cbef048a81553b040c9358e23341180
Naftali Holland <naftaliholland01@gmail.com>
2024-01-30 16:19:26 +0300

    Added a collon

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         response = response.read()
 
-        print("Body response")
+        print("Body response:")
         print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
         print("\t- utf8 content: {}".format(response.decode("utf-8")))

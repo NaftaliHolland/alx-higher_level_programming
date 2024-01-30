83296766460a2b7360a15a33e27b0f73df52dfcb
Naftali Holland <naftaliholland01@gmail.com>
2024-01-30 16:17:27 +0300

    A script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" fetches hppts://alx-intranet.hbtn.io/status """
+
+import urllib.request
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        response = response.read()
+
+        print("Body response")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode("utf-8")))

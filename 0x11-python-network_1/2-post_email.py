5a464ea59265fd47360be54fbddd58f22c8387c0
Naftali Holland <naftaliholland01@gmail.com>
2024-02-01 16:03:37 +0300

    Sends a post request to a url with an email as a parameter and prints the body of the response

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+""" Sends a POST request to a URL passed """
+
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Create dictionary with values
+    data = dict()
+    data["email"] = email
+
+    # Encode and parse the dictionary
+    data = urllib.parse.urlencode(values)
+    data = data.encode("ascii")
+
+    # Make request
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        body = response.read().decode("utf-8")
+        print(body)

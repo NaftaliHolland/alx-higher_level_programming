a48847c57d79dbdd2962178d4c32f8bdd6e59564
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2024-06-08 13:16:29 +0300

    Refactored code to follow standard

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,10 +7,11 @@ class Rectangle {
       this.height = h;
     }
   }
-  print() {
-    for(let i = 0; i < this.height; i++) {
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
       let line = '';
-      for(let j = 0; j < this.width; j++) {
+      for (let j = 0; j < this.width; j++) {
         line += 'X';
       }
       console.log(line);
